fdbd652ef534de704b611d381c3d1df240e19158
Rafael Torres <rafaelmadureiratorres@outlook.com>
2026-09-16 09:46:37 -0300

    add implementation of wait program to demonstrate process synchronization

---
+++ wait.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+int main(){
+    int pid = fork();
+    int status;
+
+    if(pid > 0){
+
+        printf("\nProcesso pai esperando o filho acabar\n");
+        wait(&status);
+
+        if(WIFEXITED(status)){
+            printf("\nProcesso filho encerrou normalmente\n");
+        }
+
+    } else if(pid == 0){
+        
+        printf("\nCódigo do Filho");
+        exit(0);
+    
+    }
+
+    return 0;
+}
